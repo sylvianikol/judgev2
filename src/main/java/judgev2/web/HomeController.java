9519ebdae80033bca4f556289b096de561aaf5ac
sylvianikol <silviaadvert@gmail.com>
2020-11-21 11:09:40 +0200

    added HomeController

---
+++ src/main/java/judgev2/web/HomeController.java
@@ -0,0 +1,19 @@
+package judgev2.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+}
+
